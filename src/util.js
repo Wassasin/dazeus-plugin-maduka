3eb7cfc84013a7d4ce6c6127d692a53355735f3d
Ruben Nijveld <ruben@gewooniets.nl>
2016-03-20 19:05:13 +0100

    Fix imports

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import util from 'util';
+
 export function printProduct(product) {
-  return util.format('%s  (%s; € %s, %s) ', product.name, product.unit, numeral(product.price).format('0,00'), result.id);
+  return util.format('%s (%s; € %s, %s) ', product.name, product.unit, numeral(product.price).format('0,00'), result.id);
 }
