dc574233eb7b37ea0ed9843b96309d93f956ce50
Ruben Nijveld <ruben@gewooniets.nl>
2016-03-20 19:19:59 +0100

    Set language

--- src/handlers/search.js
+++ src/handlers/search.js
@@ -2,6 +2,7 @@ import * as storage from '../storage';
 import Api from '../api';
 import _ from 'lodash';
 import {setSearch} from '../searches';
+import {printProduct} from '../util';
 
 export default function search(args, origin, reply) {
   let api = new Api();
@@ -21,8 +22,7 @@ export default function search(args, origin, reply) {
 
     _.chain(products)
       .slice(0, 10)
-      .map(x => i++ + ": " + x.id + " " + x.price + " " + x.name + " (" 
-                + x.unit + ")")
+      .map(x => printProduct(x))
       .each(x => reply(x))
       .value();
   });
--- src/util.js
+++ src/util.js
@@ -2,5 +2,11 @@ import util from 'util';
 import numeral from 'numeral';
 
 export function printProduct(product) {
-  return util.format('%s (%s; € %s, %s) ', product.name, product.unit, numeral(product.price).format('0,00'), result.id);
+  return util.format(
+    '%s (%s; %s)',
+    product.name,
+    product.unit,
+    numeral(product.price).format('$ 0.00'),
+    product.id
+  );
 }
