9c93c1a7fb666ea09c8537fbfd3c432fe9ecdfdd
Wouter Geraedts <git@woutergeraedts.nl>
2016-03-21 10:12:45 +0100

    Added import of 'current' list; fixed various issues

--- src/handlers/import.js
+++ src/handlers/import.js
@@ -6,23 +6,36 @@ import _ from 'lodash';
 export default function importlist(args, origin, reply) {
   let api = new Api();
   if(args.length == 0) {
-    reply("Please specify which list to import. i.e. }ah import 1");
+    reply("Please specify which list to import. i.e. }ah import lijstnaam (use 'current' for shopping basket)");
+    return;
   }
-  let listName = args[0];
+  let list = { name: args.join(" ") };
 
   api.login(config.username, config.password, () => {
-    api.listLists(list => {
-      let candidate = _.find(list, x => x.name === listName);
-
-      if(candidate === undefined) {
-        reply("List " + listName + " not found");
-        return;
-      }
-
-      api.list(candidate.id, products => {
+    let import_f = (list => {
+      api.list(list.id, products => {
         _.each(products, storage.addToList);
-        reply("Imported " + products.length + " products from list " + listName + " [id: "+ candidate.id + "]");
+        reply("Imported " + products.length + " products from list " + list.name + " [id: "+ list.id + "]");
       });
     });
+
+    if(list.name === "current")
+    {
+      list.id = 0;
+      import_f(list);
+    }
+    else
+    {
+      api.listLists(lists => {
+        let candidate = _.find(lists, x => x.name === list.name);
+
+        if(candidate === undefined) {
+          reply("List " + list.name + " not found");
+          return;
+        }
+
+        import_f(candidate);
+      });
+    }
   });
 }
